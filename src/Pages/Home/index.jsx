9b2b2231108cb610d59f91d29afa2e2c06e175c8
Kengya <mkengya@gmail.com>
2024-08-04 19:28:15 -0500

    change folder

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,34 @@
 import "../../App.css";
-import { Card } from "../../components/Card";
+import { CardUI } from "../../components/ui/CardUI";
+import { URLAPI } from "../../services/Api";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { Layout } from "../../components/Layout";
 
 export const Home = () => {
+    const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchAPI() {
+      try {
+        const response = await axios.get(URLAPI);
+        setData(response.data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        console.info("finalizado");
+      }
+    }
+    fetchAPI();
+  }, []);
   return (
-    <Layout>
-      Home
-      <Card />
-    </Layout>
+    <>
+      <Layout>
+        <div className="grid grid-cols-4 gap-3 w-full max-w-screen-lg">
+          {data?.map((item) => (
+            <CardUI key={item.id} item={item} />
+          ))}
+        </div>
+      </Layout>
+    </>
   );
 };
